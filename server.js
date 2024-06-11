9ef945fe94eaee3cf410f9cc1ae074af5bbef4b7
isaackouka225 <isaacsavadogo225@gmail.com>
2024-06-11 12:21:57 +0200

    POST fonctionality

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const fs = require('fs');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.json());
 
 app.get('/articles', (req, res) => {
     fs.readFile('database.json', (err, data) => {
@@ -9,6 +12,28 @@ app.get('/articles', (req, res) => {
             res.status(500).send('Error reading database');
         } else {
             res.send(JSON.parse(data));
+        }
+    });
+});
+
+app.post('/articles', (req, res) => {
+    fs.readFile('database.json', (err, data) => {
+        if (err) {
+            res.status(500).send('Error reading database');
+        } else {
+            const articles = JSON.parse(data);
+            const newArticle = {
+                id: articles.length + 1,
+                ...req.body
+            };
+            articles.push(newArticle);
+            fs.writeFile('database.json', JSON.stringify(articles), err => {
+                if (err) {
+                    res.status(500).send('Error saving to database');
+                } else {
+                    res.status(201).send(newArticle);
+                }
+            });
         }
     });
 });
